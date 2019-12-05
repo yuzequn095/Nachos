ca0653af83627174599cd7d6e1e063eabab3b88f
Yunshu Gao <yug129@ucsd.edu>
2019-12-05 01:07:34 -0800

    fix lock

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -142,12 +142,12 @@ public class VMProcess extends UserProcess {
 				VMKernel.pagesAvailableMutex.acquire();
 				// Check if there's no free physical pages
 				if (UserKernel.pagesAvailable.isEmpty()) {
-					VMKernel.pagesAvailableMutex.release();
 					System.out.println("Run out of physical memory without swap vpn: " + vpn);
 					// TODO evict page
 					ppn = evictPage();
 					if (ppn < 0) {
 						System.out.println("Evict unsuccessful!");
+						VMKernel.pagesAvailableMutex.release()
 						return;
 					}
 				} else {
