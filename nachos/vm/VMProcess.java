fdbf647bf9bc8fa33dc1f415b92c8d79badf0367
Yunshu Gao <yug129@ucsd.edu>
2019-12-06 00:18:16 -0800

    reduce lock coverage

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -76,7 +76,7 @@ public class VMProcess extends UserProcess {
 	 * Case2: stack/heap page, initialize with 0 if clean, load from swap if dirty.
 	 */
 	private void handlePageFault(int badVaddr) {
-		VMKernel.managerLock.acquire();
+		// VMKernel.managerLock.acquire();
 		System.out.println("Process " + pid + " start to handle page fault!");
 		int vpn = Processor.pageFromAddress(badVaddr);
 		System.out.println("Vpn at start of handlePageFault: " + vpn);
@@ -97,7 +97,9 @@ public class VMProcess extends UserProcess {
 		if (UserKernel.pagesAvailable.isEmpty()) {
 			System.out.println("Run out of physical memory without swap vpn: " + vpn);
 			// TODO evict page
+			VMKernel.managerLock.acquire();
 			ppn = evictPage();
+			VMKernel.managerLock.release();
 			if (ppn < 0) {
 				System.out.println("Evict unsuccessful!");
 				VMKernel.pagesAvailableMutex.release();
